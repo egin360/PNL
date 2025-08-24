a01087a4330540e0cc0c77d20b0c797ae7be7d97
egin360 <i.aranguren86@gmail.com>
2025-08-25 01:44:22 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Este archivo contiene la configuración y NO se sube a GitHub.
+const firebaseConfig = {
+  apiKey: "AIzaSyByc7XLgYS2hPRLlFI-i7YbkGj6cNOhU3s",
+ // apiKey: "AIzaSyD34L8aMEbxgSBy56OWIWGklRiq_-dnxe4",
+  authDomain: "alrm-egin360.firebaseapp.com",
+  databaseURL: "https://alrm-egin360-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "alrm-egin360",
+  storageBucket: "alrm-egin360.firebasestorage.app",
+  messagingSenderId: "631536838875",
+  appId: "1:631536838875:web:0850c1fe09cf3a998644fe"
+};
